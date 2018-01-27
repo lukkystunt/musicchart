23fea3459fb03592e7e91c78fdcad6a0ac371d21
Alebiosu Lukmon <alebiosulukmon@Alebiosus-MacBook-Pro.local>
2018-01-27 13:46:21 +0100

    remove unused classes

--- app/shared/services/deezer.service.ts
+++ app/shared/services/deezer.service.ts
@@ -1,22 +1,15 @@
-import { Component, Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { HttpClient} from "@angular/common/http";
 import "rxjs/add/operator/map";
-import { isAndroid, isIOS, device, screen } from "platform";
 
 
 @Injectable()
 export class DeezerService {
-    private _baseUrl = 'http://127.0.0.1:8080';
-
-    private _imageBaseUrl = 'http://127.0.0.1:8080/assets/';
     
     private _deezerApiBaseUrl = 'https://api.deezer.com/';
 
     public constructor(private _http: HttpClient) {
-        if(isAndroid){
-            this._baseUrl = 'http://10.0.2.2:8080'
-            this._imageBaseUrl = 'http://10.0.2.2:8080/assets/';
-        }
+
     }
 
     getChartFromDeezer(limit) {
